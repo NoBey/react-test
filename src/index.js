a2bcbe77f6a5ad72a002e56e20c30360ec0dde25
NoBey <786964300@qq.com>
2016-10-31 21:49:55 +0800

    s

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component}  from 'react'
+import {render} from 'react-dom'
+// import { Router, Route, Link, browserHistory } from 'react-router'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import MyAwesomeReactComponent from './MyAwesomeReactComponent.js';
+import RaisedButton from 'material-ui/RaisedButton';
+import BottomNavigation from 'material-ui/BottomNavigation';
+import {
+    Card,
+    CardActions,
+    CardHeader,
+    CardMedia,
+    CardTitle,
+    CardText
+} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import Toggle from 'material-ui/Toggle';
 
-class HelloMessage extends React.Component {
-  render() {
-    return <div>Hello {this.props.name}</div>;
-  }
-}
 
-ReactDOM.render(
-  <HelloMessage name="John" />,
-  document.getElementById('app')
-);
+render(<App />, document.getElementById('app'))
